46ad2ef178b88a06aad7be104cf3c619f0b785cb
nawaid <iqbalnawaid15@gmail.com>
2024-03-14 12:11:14 +0530

    worked on task with work group

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -21,6 +22,7 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post("/api/userDetails/login", userDetailsController.loginUser);
 
