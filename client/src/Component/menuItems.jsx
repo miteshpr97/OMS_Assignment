5d8272453e59bfde76c94fb037b14cbc35922d26
miteshpr97 <miteshpradhan97@gmail.com>
2024-04-01 14:20:55 +0530

    add

--- client/src/Component/menuItems.jsx
+++ client/src/Component/menuItems.jsx
@@ -11,8 +11,6 @@ import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import AddAlertIcon from '@mui/icons-material/AddAlert';
 
 
-
-
 export const menuItems = [
   { text: "Home", icon: <HomeIcon />, path: "/"},
   { text: "Employee", icon: <AppRegistrationIcon/>, path: "/registeration"},
@@ -25,6 +23,87 @@ export const menuItems = [
   { text: "Alert", icon: <AddAlertIcon /> , path: "/CreateAlert" },
   { text: "Report", icon: <ReportIcon  />, path: "/report" },
 ];
+
+
+
+
+
+
+
+
+// import HomeIcon from '@mui/icons-material/Home';
+// import GroupsIcon from '@mui/icons-material/Groups';
+// import AssignmentIcon from '@mui/icons-material/Assignment';
+// import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
+// import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+// import ViewListIcon from '@mui/icons-material/ViewList';
+// import ReportIcon from '@mui/icons-material/Report';
+// import GroupAddIcon from '@mui/icons-material/GroupAdd';
+// import AddAlertIcon from '@mui/icons-material/AddAlert';
+
+// const isUserData = false;
+
+// let menuItems = [
+//   { text: "Home", icon: <HomeIcon />, path: "/"},
+//   { text: "Employee", icon: <AppRegistrationIcon/>, path: "/registeration"},
+//   { text: "Master", icon: <ViewListIcon/>, path: "/master"},
+//   { text: "Team", icon: <GroupAddIcon/>, path: "/team" },
+//   { text: "My Team", icon: <GroupsIcon/>, path: "/myteam" },
+//   { text: "Assignment", icon: <AssignmentIcon />, path: "/assignment" },
+//   { text: "My Task", icon: <AssignmentTurnedInIcon/> , path: "/view-assignment" },
+//   { text: "Team Task", icon: <AssignmentTurnedInIcon/> , path: "/teamTask" },
+//   { text: "Alert", icon: <AddAlertIcon /> , path: "/CreateAlert" },
+//   { text: "Report", icon: <ReportIcon  />, path: "/report" },
+// ];
+
+// if (!isUserData) {
+//   menuItems = menuItems.filter(item => (
+//     item.text === "Master" ||
+//     item.text === "Team" ||
+//     item.text === "My Team" ||
+//     item.text === "Assignment"
+//   ));
+// }
+
+// export { menuItems };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
