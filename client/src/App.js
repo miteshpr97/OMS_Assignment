a44e09c9d3601fcf147565a9a8b2bfa7e82ff5e9
miteshpr97 <miteshpradhan97@gmail.com>
2024-04-04 14:57:21 +0530

    add authenticate

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,3 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Login from "./Component/loginPage/Login";
-import Signup from "./Component/signup/Signup";
-import Home from "./Page/Home/Home";
-import RegisterPage from "./Page/NewEmployee/RegisterPage";
-import Master from "./Page/Master/Master";
-import TeamMember from "./Page/Team/AllTeamMembers";
-import MyTeam from "./Page/MyTeam/MyTeam";
-import Create from "./Page/Team/CreateGroup/Create";
-import NewAssignment from "./Page/Assignment/NewAssignment";
-import MyTask from "./Page/MyTask/ViewAssignment";
-import TeamTask from "./Page/TeamTask/TeamTask";
-import Report from "./Page/Report/Report";
-
-import PrivateRoute from "./PrivateRoute/PrivateRoute";
-import CreateAlert from "./Page/Alert/CreateAlert";
-import ForgotPassword from "./Component/forgotPassword/ForgotPassword";
-import ResetPassword from "./Component/forgotPassword/ResetPassword";
-
-
-export default function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/loginpage" element={<Login />}></Route>
-        <Route path="/signuppage" element={<Signup />}></Route>
-        <Route path="/forgetPassword" element={<ForgotPassword/>}></Route>
-        <Route path="/ResetPassword/:token" element={<ResetPassword/>}></Route>
-        <Route path="/" element={<PrivateRoute />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/registeration" element={<RegisterPage />}></Route>
-          <Route path="/master" element={<Master />} />
-          <Route path="/createteam/:EmployeeID" element={<Create />}></Route>
-          <Route path="/team" element={<TeamMember />}></Route>
-          <Route path="/myteam" element={<MyTeam />}></Route>
-          {/* <Route path="/myteam" element={<MyTeam />}></Route> */}
-          <Route path="/assignment" element={<NewAssignment />}></Route>
-          <Route path="/view-assignment" element={<MyTask />}></Route>
-          <Route path="teamTask" element={<TeamTask />}></Route>
-          <Route path="CreateAlert" element={<CreateAlert />}></Route>
-          <Route path="/report" element={<Report />}></Route>
-        
-
-        </Route>
-      </Routes>
-    </>
-  );
-}
-
-
-
 // import React from "react";
 // import { Routes, Route } from "react-router-dom";
 // import Login from "./Component/loginPage/Login";
@@ -64,10 +12,12 @@ export default function App() {
 // import MyTask from "./Page/MyTask/ViewAssignment";
 // import TeamTask from "./Page/TeamTask/TeamTask";
 // import Report from "./Page/Report/Report";
-// import CreateAlert from "./Page/Alert/CreateAlert";
-// import PrivateRoute from "./PrivateRoute/PrivateRoute";
 
-// const userRole = JSON.parse(sessionStorage.getItem("userData"));
+// import PrivateRoute from "./PrivateRoute/PrivateRoute";
+// import CreateAlert from "./Page/Alert/CreateAlert";
+// import ForgotPassword from "./Component/forgotPassword/ForgotPassword";
+// import ResetPassword from "./Component/forgotPassword/ResetPassword";
+
 
 // export default function App() {
 //   return (
@@ -75,29 +25,23 @@ export default function App() {
 //       <Routes>
 //         <Route path="/loginpage" element={<Login />}></Route>
 //         <Route path="/signuppage" element={<Signup />}></Route>
+//         <Route path="/forgetPassword" element={<ForgotPassword/>}></Route>
+//         <Route path="/ResetPassword/:token" element={<ResetPassword/>}></Route>
 //         <Route path="/" element={<PrivateRoute />}>
 //           <Route path="/" element={<Home />}></Route>
-
-//           {/* Conditional rendering based on user role */}
-//           {userRole.Role === "Admin" && (
-//             <>
-//               <Route path="/registeration" element={<RegisterPage />}></Route>
-//               <Route path="/master" element={<Master />} />
-//               <Route path="/createteam/:EmployeeID" element={<Create />}></Route>
-//               <Route path="/team" element={<TeamMember />}></Route>
-//               <Route path="/CreateAlert" element={<CreateAlert />}></Route>
-//             </>
-//           )}
-//           {userRole.Role === "User" && (
-//             <>
-//               <Route path="/myteam" element={<MyTeam />}></Route>
-//               <Route path="/assignment" element={<NewAssignment />}></Route>
-//               <Route path="/view-assignment" element={<MyTask />}></Route>
-//               <Route path="/teamTask" element={<TeamTask />}></Route>
-//             </>
-//           )}
-
+//           <Route path="/registeration" element={<RegisterPage />}></Route>
+//           <Route path="/master" element={<Master />} />
+//           <Route path="/createteam/:EmployeeID" element={<Create />}></Route>
+//           <Route path="/team" element={<TeamMember />}></Route>
+//           <Route path="/myteam" element={<MyTeam />}></Route>
+//           {/* <Route path="/myteam" element={<MyTeam />}></Route> */}
+//           <Route path="/assignment" element={<NewAssignment />}></Route>
+//           <Route path="/view-assignment" element={<MyTask />}></Route>
+//           <Route path="teamTask" element={<TeamTask />}></Route>
+//           <Route path="CreateAlert" element={<CreateAlert />}></Route>
 //           <Route path="/report" element={<Report />}></Route>
+        
+
 //         </Route>
 //       </Routes>
 //     </>
@@ -105,3 +49,62 @@ export default function App() {
 // }
 
 
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Component/loginPage/Login";
+import Signup from "./Component/signup/Signup";
+import Home from "./Page/Home/Home";
+import RegisterPage from "./Page/NewEmployee/RegisterPage";
+import Master from "./Page/Master/Master";
+import TeamMember from "./Page/Team/AllTeamMembers";
+import MyTeam from "./Page/MyTeam/MyTeam";
+import Create from "./Page/Team/CreateGroup/Create";
+import NewAssignment from "./Page/Assignment/NewAssignment";
+import MyTask from "./Page/MyTask/ViewAssignment";
+import TeamTask from "./Page/TeamTask/TeamTask";
+import Report from "./Page/Report/Report";
+import CreateAlert from "./Page/Alert/CreateAlert";
+import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import { useSelector } from "react-redux";
+
+
+
+export default function App() {
+ // const userRole = JSON.parse(sessionStorage.getItem("userData"));//
+  const userRole = useSelector((state) => state.user.userData);
+  return (
+    <>
+      <Routes>
+        <Route path="/loginpage" element={<Login />}></Route>
+        <Route path="/signuppage" element={<Signup />}></Route>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/" element={<Home />}></Route>
+          {userRole && (
+            <>
+              {userRole.Role === "Admin" && (
+                <>
+                  <Route path="/registeration" element={<RegisterPage />}></Route>
+                  <Route path="/master" element={<Master />} />
+                  <Route path="/createteam/:EmployeeID" element={<Create />}></Route>
+                  <Route path="/team" element={<TeamMember />}></Route>
+                  <Route path="/CreateAlert" element={<CreateAlert />}></Route>
+                </>
+              )}
+              {userRole.Role === "User" && (
+                <>
+                  <Route path="/myteam" element={<MyTeam />}></Route>
+                  <Route path="/assignment" element={<NewAssignment />}></Route>
+                  <Route path="/view-assignment" element={<MyTask />}></Route>
+                  <Route path="/teamTask" element={<TeamTask />}></Route>
+                </>
+              )}
+            </>
+          )}
+
+          <Route path="/report" element={<Report />}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
